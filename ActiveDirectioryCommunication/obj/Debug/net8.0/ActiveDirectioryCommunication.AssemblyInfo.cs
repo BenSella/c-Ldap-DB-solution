c8d342c545592346ebea87d07aa14c4595008bdd
Ben.Sella <bensella@gmail.com>
2024-09-17 10:18:15 +0300

    first commit

--- ActiveDirectioryCommunication/obj/Debug/net8.0/ActiveDirectioryCommunication.AssemblyInfo.cs
+++ ActiveDirectioryCommunication/obj/Debug/net8.0/ActiveDirectioryCommunication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActiveDirectioryCommunication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79d0622da464442666d5ffa2b21a2d4ce036b92")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActiveDirectioryCommunication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActiveDirectioryCommunication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
